345d861e01a0541c77b0561d23ade9311c943d36
yordan <sularome@gmail.com>
2022-01-22 20:48:31 +0100

    fix: eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -62,6 +62,7 @@ function App() {
     return () => {
       scheduler.current?.unsubscribe(moveToNextNote);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
